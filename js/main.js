7d47deeedbd22aeff247ac5486c0344a7d688533
Dan Snider <dansnider@gmail.com>
2015-04-29 15:57:10 -0400

    programs list

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 var app = angular.module('myApp', []);
 
-app.controller('PlayerController', ['$scope', function($scope) {
+var apiKey = 'MDE5MDA1ODI1MDE0MzAzMzYyMzlmNzdiNA001',
+		nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
-}]);
+app.controller('PlayerController', function($scope, $http){
+
+	$http({
+		method: 'JSONP',
+		url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+	}).success(function(data, status) {
+		console.log(data)
+		$scope.programs = data.list.story;
+		// success function
+	}).error(function(data, status) {
+		// error function
+	})
+
+})
+
+
+// app.controller('PlayerController', ['$scope', function($scope) {
+// 	$scope.playing = false;
+// 	$scope.audio = document.createElement('audio');
+// 	$scope.audio.src = '/media/npr.mp4'
+// 	$scope.play = function() {
+// 		$scope.audio.play();
+// 		$scope.playing = true;
+// 	};
+// 	$scope.stop = function() {
+// 		$scope.audio.stop();
+// 		$scope.playing = false;
+// 	};
+// 	$scope.audio.addEventListener('ended', function() {
+// 		$scope.$apply(function() {
+// 			$scope.stop()
+// 		});
+// 	});
+// }]);
 
 app.controller('RelatedController', ['$scope', function($scope) {
 
