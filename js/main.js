adfd83d4dbc687a27b388f4d2346d4890941c53d
Dan Snider <dansnider@gmail.com>
2015-04-29 14:39:51 -0400

    controllers added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 var app = angular.module('myApp', []);
+
+app.controller('PlayerController', ['$scope', function($scope) {
+
+}]);
+
+app.controller('RelatedController', ['$scope', function($scope) {
+
+}]);
